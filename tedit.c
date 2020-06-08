11acf0953b476ece7732297545e636645775cde4
sidou01 <sidou010@outlook.com>
2020-06-08 18:49:32 +0100

    tedit can read files

--- tedit.c
+++ tedit.c
@@ -21,6 +21,7 @@ void draw_header(const char *filename)
     attroff(A_STANDOUT);
 }
 
+/* interpret arrow/command keys and print the rest */
 void process_keys(int *position_y, int *position_x, int key)
 {
     switch(key)
@@ -41,7 +42,40 @@ void process_keys(int *position_y, int *position_x, int key)
             if(*position_x < COLS - 1)
                 *position_x = *position_x + 1;
             break;
+        default:
+            addch(key);
     }
+}
+
+void process_editor_commands(const char *key_name)
+{
+    if(strcmp(key_name,":") == 0)
+    {
+        const char *next_key = keyname(getch());
+        switch(*next_key)
+        {
+            case 'q':
+                endwin();
+                exit(1);
+                break;
+            case 'w':
+                /* save the file and update status bar*/
+                break;
+
+        }
+        if(strcmp(next_key, "q") == 0)
+        {
+        }
+        /* handle file save with :w */
+
+    }
+    /* printw("HELLO: %s", key_name); */
+    if(strcmp(key_name, KEY_EXIT_EDITOR) == 0)
+    {
+        endwin();
+        exit(0);
+    }
+
 }
 
 void init_editor()
@@ -49,26 +83,45 @@ void init_editor()
     int key;
     int position_x = 0;
     int position_y= 1;
+
     while(1)
     {
         move(position_y, position_x);
         key = getch();
         process_keys(&position_y, &position_x, key);
-
         const char *key_name = keyname(key);
-        if(strcmp(key_name, KEY_EXIT_EDITOR) == 0)
-        {
-            endwin();
-            exit(0);
-        }
+        process_editor_commands(key_name);
+    }
+}
+
+void read_file(char *filename)
+{
+    FILE *file;
+    char *file_content;
+    int file_size;
+
+    if((file = fopen(filename, "r")) == NULL)
+    { 
+        endwin();
+        printf("Error opening the file \"%s\"\n", filename);
+        exit(1);
     }
 
+    fseek(file, 0, SEEK_END);
+    file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    file_content = malloc(sizeof *file_content * file_size);
+    if(file_content == NULL) { printf("Error allocating memory to a file\n"); exit(1); }
+    fread(file_content, sizeof(char), file_size, file);
+    printw("%s", file_content);
 }
+
 
 int main(int argc, char *argv[])
 {
-
     char *filename;
+
     initscr();
     noecho();
     keypad(stdscr, TRUE);
@@ -76,10 +129,11 @@ int main(int argc, char *argv[])
     if(argv[1])
         filename = argv[1];
     else
-        filename = "<<new file>>";
+        filename = "file";
+
 
     draw_header(filename);
+    read_file(filename);
     init_editor();
-
     return 0;
 }
